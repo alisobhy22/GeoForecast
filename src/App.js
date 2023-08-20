004a19cf3c36411c8943c1e90379a40b2303b8a8
alisobhy22 <64151548+alisobhy22@users.noreply.github.com>
2023-08-21 02:36:36 +0300

    got canvas to work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { MyResponsiveChoropleth  } from './Map'
 
-
-
+import data from './data.json'
 function App() {
+
+  
+
+
+
+
   return (
-   <h1>app</h1>
+    <div className="m-50 w-screen outline h-screen flex justify-center items-center">
+      <div className='w-2/3 h-screen'> <MyResponsiveChoropleth data={data} /></div>
+      <div className='w-1/3'>hello</div>
+    </div>
   );
 }
 
